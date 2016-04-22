09b01f688e56279e09017508527e314c43eda979
Alex Zherdev <alex.zherdev@gmail.com>
2016-04-22 13:17:20 +0300

    API support to create players

--- src/api/models/player.js
+++ src/api/models/player.js
@@ -1,11 +1,19 @@
 'use strict';
 
 
+let moment = require('moment');
+
 let knex = require('../db');
 
 let Player = {
   all() {
     return knex.select('*').from('players');
+  },
+
+  create(name) {
+    let m = moment();
+    let date = m.subtract(m.utcOffset(), 'minutes').toDate();
+    return knex.insert({ name, created_at: date, updated_at: date }).into('players');
   }
 };
 
