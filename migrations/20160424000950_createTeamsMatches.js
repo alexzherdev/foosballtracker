d9992ac6afd5fbabe6db8b94a74e3114886743af
Alex Zherdev <alex.zherdev@gmail.com>
2016-04-24 00:16:56 +0300

    Teams and matches schema

---
+++ migrations/20160424000950_createTeamsMatches.js
@@ -0,0 +1,25 @@
+exports.up = function(knex, Promise) {
+  var teams = knex.schema.createTable('teams', function(table) {
+    table.increments();
+    table.string('name');
+    table.timestamps();
+  });
+  var matches = knex.schema.createTable('matches', function(table) {
+    table.increments();
+    table.integer('team1_id');
+    table.integer('team2_id');
+    table.integer('team1_score');
+    table.integer('team2_score');
+    table.string('match_type'); // 1v1, 1v2, 2v2
+    table.timestamps();
+  });
+
+  return Promise.all([teams, matches]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('matches'),
+    knex.schema.dropTable('teams')
+  ]);;
+};
