f32788050c149e44a8d8b23709a75f4761f1836e
Alex Zherdev <alex.zherdev@gmail.com>
2016-04-24 16:21:58 +0300

    Add coverage tools

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,14 @@ module.exports = function(config) {
   config.set({
     browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
     files: [
-      { pattern: 'tests.webpack.js', watched: false },
+      { pattern: 'tests.webpack.js', watched: false }
     ],
     frameworks: ['jasmine'],
     preprocessors: {
       'tests.webpack.js': ['webpack'],
+      'src/**/*.js': ['coverage']
     },
-    reporters: ['dots'],
+    reporters: ['progress', 'coverage'],
     singleRun: true,
     webpack: {
       module: {
@@ -20,13 +21,25 @@ module.exports = function(config) {
         loaders: [
           { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
         ],
-        plugins: ['babel-runtime']
+        plugins: ['babel-runtime'],
+        postLoaders: [{ //delays coverage til after tests are run, fixing transpiled source coverage error
+          test: /\.js$/,
+          exclude: /(test|node_modules)\//,
+          loader: 'istanbul-instrumenter'
+        }]
       },
       plugins: [new RewireWebpackPlugin()],
       watch: true
     },
     webpackServer: {
-      noInfo: true,
+      noInfo: true
     },
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcov', subdir: 'report-lcov' }
+      ]
+    }
   });
 };
